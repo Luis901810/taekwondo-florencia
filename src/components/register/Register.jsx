8859e01c66d7e7905e2c5f2468704fd8fc020d98
luis901810 <luis901810@gmail.com>
2024-04-19 22:00:10 -0500

    modificaciones carrusel

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,25 +1,29 @@
-
+import styles from "./Register.module.css"
 
 export default function Register () {
 
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Bienvenidos al Registro</h1>
-        <form action="">
+        <form className={styles.form} action="">
         
-        <label htmlFor="nombre">Nombre</label>
+        <label htmlFor="nombre"> Nombre </label>
         <input type="text" name="nombre" id="nombre"  placeholder="Nombre"/>
-        <label htmlFor="apellido">Apellido</label>
+        <label htmlFor="apellido"> Apellido </label>
         <input type="text" name="apellido" id="apellido" placeholder="Apellido"/>
-        <label htmlFor="direccion">Direccion</label>
+        <label htmlFor="direccion"> Direccion </label>
         <input type="text" name="direccion" id="direccion" placeholder="lugar de residencia" />
-        <label htmlFor="correo">Correo</label>
+        <label htmlFor="correo"> Correo </label>
         <input type="email" name="correo" id="correo" placeholder="correo electronico" />
-        <label htmlFor="password">Contraseña</label>
+        <label htmlFor="password"> Contraseña </label>
         <input type="password"  name="password" id="password" placeholder="nueva contraseña"/>
-        <label htmlFor="rep password">Confirme Contraseña</label>
+        <label htmlFor="rep password"> Confirme Contraseña </label>
         <input type="password" name="rep password" id="rep password" placeholder="confirme contraseña" />
-        <button>Registrar</button>
+        <label >
+            <i className="bx bx-user"></i>
+            <input type="text" placeholder="prueba" />
+        </label>
+        <button> Registrar </button>
 
      </form>
 
