6545210126fc61fffcdf7c92dbb36cf3cdae0ddc
luis901810 <luis901810@gmail.com>
2024-04-25 12:42:22 -0500

    subo arreglos del registro realizados

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,15 +1,24 @@
+
+import { useState } from "react"
 import styles from "./Register.module.css"
 
 export default function Register () {
 
+        const [ isLoginForm, setIsLoginForm ] = useState(true);
+
+        const toggleFormVisibility = () =>{
+            setIsLoginForm(!isLoginForm);
+        }
+
+
     return (
         <div className={styles.container}>
-            <div className={styles.information}>
+            <div className={`${styles.information}  ${isLoginForm ? styles.hide :"" } register`}>
                 <div className={styles.infor}>
                 <div className={styles.childs}>
                     <h2>Bienvenidos</h2>
                     <p>Para unirte a nuestra comudidad de Teakwondo te invitamos a Iniciar sesion con tus datos</p>
-                    <input type="button" value="Iniciar sesion" />
+                    <input type="button" value="Iniciar sesion" onClick={toggleFormVisibility}  id="sign-in"/>
                 </div>
 
                 </div>
@@ -42,6 +51,47 @@ export default function Register () {
             </div>
             </div>
             </div>
+
+            {/* //---------------------------------------------------------------------------------- */}
+
+            <div className={`${styles.information} ${isLoginForm ? "": styles.hide}  login`}>
+                <div className={styles.infor}>
+                <div className={styles.childs}>
+                    <h2>!! Bienvenidos Nuevamente</h2>
+                    <p>Si aun no tienes una cuenta por favor registrate aqui</p>
+                    <input type="button" value="Registrarse" onClick={toggleFormVisibility} id="sign-up"/>
+                </div>
+
+                </div>
+            
+
+            <div className={styles.formInf}>
+                <div className={styles.formChilds}>
+                    <h2>Iniciar Sesion</h2>
+                    <div className={styles.icon}>
+                    <i className='bx bxl-google'></i>
+                    <i className='bx bxl-facebook-circle'></i>
+                    </div>
+                <p>O inicia sesion con una cuenta</p>
+
+            <form className={styles.form}  action="">
+                <label >
+                <i className='bx bx-envelope'></i>
+                <input type="email" placeholder="Correo electronico" />
+                </label>
+                <label htmlFor="">
+                <i className='bx bx-lock-alt'></i>
+                <input type="password" placeholder="**********" />
+                </label>
+                <input className={styles.boton} type="submit" value="Iniciar Sesion" />
+            </form>
+            </div>
+            </div>
+            </div>
     </div>
+
+
+
+
     )
 }
